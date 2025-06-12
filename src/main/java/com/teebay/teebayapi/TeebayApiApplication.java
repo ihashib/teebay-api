d228ddf7c16d9181287d4df3630af611e51fa72f
ihashib <ihashib2@gmail.com>
2025-06-12 11:54:59 +0600

    init be

---
+++ src/main/java/com/teebay/teebayapi/TeebayApiApplication.java
@@ -0,0 +1,13 @@
+package com.teebay.teebayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeebayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeebayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teebay/teebayapi/TeebayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teebay.teebayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeebayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
